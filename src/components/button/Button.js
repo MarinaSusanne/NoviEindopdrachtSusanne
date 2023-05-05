b4df58b0d76bfc5cd8528be21c993f811d773c23
MarinaSusanne <susanne.hagen@live.nl>
2023-05-05 14:18:53 +0200

    continued on LoginPage and button component

---
+++ src/components/button/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Button.module.css';
+
+
+export default function Button({buttonText, onClick, buttonStyle, buttonType}) {
+
+    return(
+        <button
+            type={buttonType}
+            onClick={onClick}
+            className={`${styles[buttonStyle]}`}>{buttonText}</button>
+    )
+
+
+}
--- src/pages/logIn/LogIn.js
+++ src/pages/logIn/LogIn.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './LogIn.module.css';
 import WhiteBox from '../../components/whiteBox/WhiteBox';
-import Navigation from '../../components/navigation/Navigation';
 import FormInput from '../../components/formInput/FormInput';
 import Button from '../../components/button/Button';
 import {useForm} from "react-hook-form";
@@ -9,7 +8,7 @@ import {Link} from "react-router-dom";
 
 
 export default function LogIn() {
-    const {register, handleSubmit, formState:{errors}} = useForm({mode:"onChange"});
+    const {register, handleSubmit, formState:{errors}} = useForm({mode:"onSubmit"});
 
     function handleFormSubmit(data){
         console.log(data)
@@ -28,7 +27,8 @@ export default function LogIn() {
                           labelText="Email:"
                           type="text"
                           id="email-field"
-                          register="email"
+                          register = {register}
+                          registerName="email"
                           validationRules= {{
                               required: {
                               value: true,
@@ -40,49 +40,33 @@ export default function LogIn() {
                           errors={errors}
                       />
 
-                      <label htmlFor="email-field">
-                          Email:
-                          <input
-                              type="text"
-                              id="email-field"
-                              {...register("email", {
-                                      required: {
-                                          value: true,
-                                          message: 'Dit veld is verplicht',
-                                      },
-                                      validate: (value) => value.includes('@') || 'Email moet een @ bevatten',
-                                  }
-                              )}
-                          />
-                          {errors.email && <p>{errors.email.message}</p>}
-                      </label>
+                      <FormInput
+                          htmlFor="password-field"
+                          labelText="Wachtwoord:"
+                          type="text"
+                          id="password-field"
+                          register={register}
+                          registerName="password"
+                          validationRules= {{
+                              required: "Dit veld is verplicht",
+                              minlength: {
+                                  value: 10,
+                                  message: "Minstens 10 karakters"
+                              },
+                              maxLength: 50,
+                          }}
+                          className="input"
+                          errors={errors}
+                      />
 
-                      <label htmlFor="password-field">
-                          Wachtwoord:
-                          <input
-                              type="text"
-                              id="password-field"
-                              {...register("password", {
-                                  required: "Dit veld is verplicht",
-                                  minlength: {
-                                      value: 10,
-                                      message: "Minstens 10 karakters"
-                                  },
-                                  maxLength: 50,
-                              })}
-                          />
-                          {errors.password && <p>{errors.password.message}</p>}
-                      </label>
                     </form>
-                   <button type="submit"> Inloggen</button>
-
+                   <Button
+                    buttonType="submit"
+                    buttonText="Inloggen"
+                    buttonStyle="buttonStyle"
+                   />
                    <p> Heb je nog geen account? <Link to="/registreer"> Klik dan hier! </Link>  </p>
-
-
-                  {/*<Button>*/}
-
-                  {/*</Button>*/}
-              </WhiteBox>
+                </WhiteBox>
               </section>
           </section>
         </body>
