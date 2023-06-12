9dbe531b512a3924c4b669434123bac5237f3680
MarinaSusanne <susanne.hagen@live.nl>
2023-06-12 18:25:35 +0200

    started with createGroupPage, need to add the requests

--- src/pages/createGroupAdmin/CreateGroupAdmin.js
+++ src/pages/createGroupAdmin/CreateGroupAdmin.js
@@ -1,12 +1,36 @@
 import styles from './CreateGroupAdmin.module.css';
 import React from 'react';
+import WhiteBox from "../../components/whiteBox/WhiteBox";
+import Innerbox from '../../components/innerGoldBox/InnerGoldBox';
+import Button from '../../components/button/Button';
+import FormInput from "../../components/formInput/FormInput";
+import {useForm} from "react-hook-form";
+import axios from "axios";
 
-function CreateGroupAdmin(props) {
+function CreateGroupAdmin() {
+
+function handleFormSubmit(){
+
+}
+
+
     return (
-        <div>
-            <h2> Test create group admin</h2>
+        <div className="outer-container">
+            <section className={styles["page-body"]}>
+                <section className="inner-container">
+                    <WhiteBox className='register-box'>
+                        <h2> Groep aanmaken</h2>
+                        <form className="group-registration" onSubmit={handleSubmit(handleFormSubmit)}>
+                            <FormInput>
+
+                            </FormInput>
+                        </form>
+                    </WhiteBox>
+                </section>
+            </section>
         </div>
-    );
+    )
+
 }
 
 export default CreateGroupAdmin;
